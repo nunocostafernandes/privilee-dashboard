97e289230db54146a2f584e43f436318f84d9cd7
Nuno Costa <nuno.costa@crank-fit.com>
2026-03-26 11:20:23 +0400

    feat: add redeploy script

---
+++ redeploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+MSG=${1:-"update"}
+source .deploy-secrets
+git add -A && git commit -m "$MSG" && git push
+sshpass -p "$VPS_PASSWORD" ssh -o StrictHostKeyChecking=no root@187.77.129.84 "
+  cd /var/www/privilee-dashboard &&
+  git pull &&
+  npm install --production=false &&
+  npm run build &&
+  pm2 restart privilee-dashboard
+"
